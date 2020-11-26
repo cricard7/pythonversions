68f8eea2d5bdd006f7c30631f62f8e12e07c0094
cricard7 <chad_ricard@hotmail.com>
2020-11-25 22:01:54 -0600

    initial

---
+++ myproject/hello.py
@@ -0,0 +1,14 @@
+#https://www.infoworld.com/article/3239675/virtualenv-and-venv-python-virtual-environments-explained.html
+
+#anaconda and managing in jupyter
+#https://janakiev.com/blog/jupyter-virtual-envs/
+
+print("venv")
+
+print("To create a python environment using venv type: python -m venv ThePathToDirectory ")
+
+print("then create a new folder beside the enironment that was created to hold your project.")
+
+print("Make a new python file in the project folder and it should automatically detect the new environment in VSCode with the extension but click bottom left to see what evironemnt is running")
+
+print("to create environments with specific versions we need virtualenv. Install with python -m pip install --user virtualenv")
