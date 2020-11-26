2393dac48fb3debace5e77c659938313a1ec38d7
cricard7 <chad_ricard@hotmail.com>
2020-11-26 12:28:57 -0600

    info on virtualenv added

--- myproject/hello.py
+++ myproject/hello.py
@@ -3,6 +3,9 @@
 #anaconda and managing in jupyter
 #https://janakiev.com/blog/jupyter-virtual-envs/
 
+#Using virtualenv to create specific virtual environment by python version
+#https://www.freecodecamp.org/news/installing-multiple-python-versions-on-windows-using-virtualenv/
+
 print("venv")
 
 print("To create a python environment using venv type: python -m venv ThePathToDirectory ")
@@ -12,3 +15,5 @@ print("then create a new folder beside the enironment that was created to hold y
 print("Make a new python file in the project folder and it should automatically detect the new environment in VSCode with the extension but click bottom left to see what evironemnt is running")
 
 print("to create environments with specific versions we need virtualenv. Install with python -m pip install --user virtualenv")
+
+print("Follow https://www.freecodecamp.org/news/installing-multiple-python-versions-on-windows-using-virtualenv/ to install multiple versions under windows)
